a0909c84b77bd27b2f5de09f53ec4b98ef191e6d
Ben Gale <ben.gale@4com.co.uk>
2015-06-13 17:14:59 +0100

    Passing Tests.

---
+++ src/BasketTest.Discounts/VoucherValidation/Offer/SingleOfferVoucherValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BasketTest.Discounts.Items;
+
+namespace BasketTest.Discounts.VoucherValidation.Offer
+{
+    public class SingleOfferVoucherValidator : IOfferVoucherValidator
+    {
+        public List<InvalidVoucher> Validate(List<Product> products, List<OfferVoucher> vouchers)
+        {
+            var validVoucher = vouchers.OrderByDescending(v => v.Value).Take(1);
+            var invalidVouchers = vouchers.Except(validVoucher)
+                .Select(voucher => new InvalidVoucher(
+                    voucher, "You may only have one offer voucher in the basket.")).ToList();
+
+            return invalidVouchers;
+        }
+    }
+}
