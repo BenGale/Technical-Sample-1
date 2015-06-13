6237697c94e9d19d4afb05d5d63715225f21029f
Ben Gale <ben.gale@4com.co.uk>
2015-06-13 18:00:09 +0100

    Passing Tests. Added more tests for this validator that I may have got ahead of myself on in the last commit.

--- test/BasketTest.Discount.UnitTests/VoucherValidation/Offer/OfferVoucherThresholdValidatorSpec.cs
+++ test/BasketTest.Discount.UnitTests/VoucherValidation/Offer/OfferVoucherThresholdValidatorSpec.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using BasketTest.Discounts.Enums;
 using BasketTest.Discounts.Items;
 using BasketTest.Discounts.VoucherValidation.Offer;
 using FluentAssertions;
@@ -34,6 +35,41 @@ namespace BasketTest.Discount.UnitTests.VoucherValidation.Offer
             };
 
             var result = _sut.Validate(testProducts, new List<OfferVoucher> {testVoucher});
+
+            result.Should().HaveCount(1);
+            result.First().Voucher.Should().Be(testVoucher);
+        }
+
+        [Test]
+        public void Validator_KeepsValidVouchers()
+        {
+            var testVoucherA = new OfferVoucher(10m, 20m);
+            var testVoucherB = new OfferVoucher(10m, 10m);
+            var testProducts = new List<Product>
+            {
+                new Product("Hat", 15m),
+            };
+
+            var result = _sut.Validate(testProducts, new List<OfferVoucher>
+                { testVoucherA, testVoucherB });
+
+            result.Should().HaveCount(1);
+            result.First().Voucher.Should().Be(testVoucherA);
+            _offerValidatorMock.Verify(m => m.Validate(It.IsAny<List<Product>>(),
+                It.Is<List<OfferVoucher>>(list => list.Count() == 1)));
+        }
+
+        [Test]
+        public void Validator_DoesntIncludeGiftVouchers()
+        {
+            var testVoucher = new OfferVoucher(10m, 20m);
+            var testProducts = new List<Product>
+            {
+                new Product("Hat", 15m),
+                new Product("Voucher", 100m, ProductCategory.GiftVoucher)
+            };
+
+            var result = _sut.Validate(testProducts, new List<OfferVoucher> { testVoucher });
 
             result.Should().HaveCount(1);
             result.First().Voucher.Should().Be(testVoucher);
