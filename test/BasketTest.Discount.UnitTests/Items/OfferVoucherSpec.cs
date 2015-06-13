536da43e5985d5b1550dd1cf895a94a648c3c35d
Ben Gale <ben.gale@4com.co.uk>
2015-06-13 15:22:07 +0100

    Failing Test. Adding a class to represent an offer voucher and its threshold req.

---
+++ test/BasketTest.Discount.UnitTests/Items/OfferVoucherSpec.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BasketTest.Discounts.Items;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BasketTest.Discount.UnitTests.Items
+{
+    [TestFixture]
+    public class OfferVoucherSpec
+    {
+        [Test]
+        public void GiftVoucher_RejectsNegativeValue()
+        {
+            Action act = () =>
+            {
+                var sut = new OfferVoucher(-10m, 10m);
+            };
+            act.ShouldThrow<ArgumentException>()
+                .WithMessage("Value cannot be negative.");
+            
+        }
+
+        [Test]
+        public void GiftVoucher_AcceptsPositiveValue()
+        {
+            Action act = () =>
+            {
+                var sut = new OfferVoucher(10m, 10m);
+            };
+            act.ShouldNotThrow();
+        }
+
+        [Test]
+        public void GiftVoucher_RejectsNegativeThreshold()
+        {
+            Action act = () =>
+            {
+                var sut = new OfferVoucher(10m, -10m);
+            };
+            act.ShouldThrow<ArgumentException>()
+                .WithMessage("Value cannot be negative.");
+
+        }
+    }
+}
