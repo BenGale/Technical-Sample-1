b2f75a3fc643dee69206fa3afa692957ade258d1
Ben Gale <ben.gale@4com.co.uk>
2015-06-13 15:23:37 +0100

    Passing Test.

---
+++ src/BasketTest.Discounts/Items/OfferVoucher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasketTest.Discounts.Items
+{
+    public class OfferVoucher
+    {
+        public readonly decimal Value;
+        public readonly decimal Threshold;
+
+        public OfferVoucher(decimal value, decimal threshold)
+        {
+            if (value < 0 || threshold < 0)
+            {
+                throw new ArgumentException("Value cannot be negative.");
+            }
+
+            Value = value;
+            Threshold = threshold;
+        }
+    }
+}
